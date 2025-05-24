2e37b0f7ab8775042e783289becc5677595f47d2
elilionel7 <elilionel7@gmail.com>
2025-05-24 10:48:58 -0500

    Add plot for Au function

--- plot_eigfunc.py
+++ plot_eigfunc.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 def plot_eigenfunctions(u_computed, u_analytic, gdata, filename=None):
     
@@ -7,10 +8,8 @@ def plot_eigenfunctions(u_computed, u_analytic, gdata, filename=None):
     u_comp_grid = u_computed.reshape(m, m)
     u_ana_grid = u_analytic.reshape(m, m)
     
-    # Create figure
     fig = plt.figure(figsize=(16, 6))
     
-    # Plot computed solution
     ax1 = fig.add_subplot(121, projection='3d')
     surf1 = ax1.plot_surface(gdata.x1, gdata.x2, u_comp_grid,
                            cmap='viridis', rstride=1, cstride=1)
@@ -30,7 +29,31 @@ def plot_eigenfunctions(u_computed, u_analytic, gdata, filename=None):
     
     plt.tight_layout()
     
-    # Save or show
+    if filename:
+        plt.savefig(filename, bbox_inches='tight', dpi=300)
+    else:
+        plt.show()
+    plt.close()
+
+
+def plot_Au_surfaces(Au_list, gdata_list, pts_list, filename=None):
+    
+    n = len(Au_list)
+    fig = plt.figure(figsize=(6 * n, 5))
+    
+    for i, (Au, gdata, pts) in enumerate(zip(Au_list, gdata_list, pts_list)):
+        Au_full_grid = np.zeros_like(gdata.x1)
+        
+        Au_full_grid[gdata.flag] = Au[:gdata.k]
+
+        ax = fig.add_subplot(1, n, i+1, projection='3d')
+        surf = ax.plot_surface(gdata.x1, gdata.x2, Au_full_grid, cmap='viridis', rstride=1, cstride=1)
+        ax.set_title(f"Au, pts={pts}")
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        fig.colorbar(surf, ax=ax, shrink=0.5, aspect=10)
+    
+    plt.tight_layout()
     if filename:
         plt.savefig(filename, bbox_inches='tight', dpi=300)
     else:
